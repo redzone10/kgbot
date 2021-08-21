38108d29b1a1a3c4ed4dce7c0c2d045e77fb6dfc
KGPROJECT <89075931+kgproject1010@users.noreply.github.com>
2021-08-21 18:38:15 +0700

    Add files via upload

---
+++ kgbot/__main__.py
@@ -0,0 +1,136 @@
+import glob
+from kgbot import bot
+from sys import argv
+from telethon import TelegramClient
+from kgbot.kgbotConfig import Var
+from kgbot.utils import load_module, start_mybot, load_pmbot
+from pathlib import Path
+import telethon.utils
+from kgbot import CMD_HNDLR
+
+THUND = Var.PRIVATE_GROUP_ID
+BOTNAME = Var.TG_BOT_USER_NAME_BF_HER
+LOAD_MYBOT = Var.LOAD_MYBOT
+logo = """
+       $$$$$$"
+      .$$$$$$"
+     .$$$$$$"
+    4$$$$$$$$$$$$$"
+   z$$$$$$$$$$$$$"
+   """ """"3$$$$$"
+         z$$$$P
+        d$$$$"
+      .$$$$$"
+     z$$$$$"
+    z$$$$P
+   d$$$$$$$$$$"
+  *******$$$"
+       .$$$"
+      .$$"
+     4$P"
+    z$"
+   zP
+  z"
+ /    kgproject1010
+
+
+
+                                                      █████████████████████████████████
+                                                      █▒▒▒▒▒▒██▒▒▒▒▒▒▒▒█▒▒▒▒▒▒▒▒▒▒▒▒▒▒█
+                                                      █▒▒▄▄▒▒██▒▒▄▄▄▄▒▒█▒▒▄▄▄▄▄▄▄▄▄▄▒▒█
+                                                      █▒▒▄▄▒▒██▒▒▄▄▒▒▒▒█▒▒▄▄▒▒▒▒▒▒▒▒▒▒█
+                                                      █▒▒▄▄▒▒██▒▒▄▄▒▒███▒▒▄▄▒▒█████████
+                                                      █▒▒▄▄▒▒▒▒▒▒▄▄▒▒███▒▒▄▄▒▒█████████
+                                                      █▒▒▄▄▄▄▄▄▄▄▄▄▒▒███▒▒▄▄▒▒██▒▒▒▒▒▒█
+                                                      █▒▒▄▄▒▒▒▒▒▒▄▄▒▒███▒▒▄▄▒▒██▒▒▄▄▒▒█
+                                                      █▒▒▄▄▒▒██▒▒▄▄▒▒███▒▒▄▄▒▒██▒▒▄▄▒▒█
+                                                      █▒▒▄▄▒▒██▒▒▄▄▒▒▒▒█▒▒▄▄▒▒▒▒▒▒▄▄▒▒█
+                                                      █▒▒▄▄▒▒██▒▒▄▄▄▄▒▒█▒▒▄▄▄▄▄▄▄▄▄▄▒▒█
+                                                      █▒▒▒▒▒▒██▒▒▒▒▒▒▒▒█▒▒▒▒▒▒▒▒▒▒▒▒▒▒█
+                                                      █████████████████████████████████
+███████████████████████████████████████████████████████████████████████████████████████████████████████████████
+█▒▒▒▒▒▒██▒▒▒▒▒▒█▒▒▒▒▒▒▒▒▒▒▒▒▒▒█▒▒▒▒▒▒▒▒▒▒▒▒▒▒█▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒███▒▒▒▒▒▒▒▒▒▒▒▒▒▒███▒▒▒▒▒▒▒▒▒▒▒▒▒▒█▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+█▒▒▄▄▒▒██▒▒▄▄▒▒█▒▒▄▄▄▄▄▄▄▄▄▄▒▒█▒▒▄▄▄▄▄▄▄▄▄▄▒▒█▒▒▄▄▄▄▄▄▄▄▄▄▄▄▒▒███▒▒▄▄▄▄▄▄▄▄▄▄▒▒███▒▒▄▄▄▄▄▄▄▄▄▄▒▒█▒▒▄▄▄▄▄▄▄▄▄▄▒▒
+█▒▒▄▄▒▒██▒▒▄▄▒▒█▒▒▄▄▒▒▒▒▒▒▒▒▒▒█▒▒▄▄▒▒▒▒▒▒▒▒▒▒█▒▒▄▄▒▒▒▒▒▒▒▒▄▄▒▒███▒▒▄▄▒▒▒▒▒▒▄▄▒▒███▒▒▄▄▒▒▒▒▒▒▄▄▒▒█▒▒▒▒▒▒▄▄▒▒▒▒▒▒
+█▒▒▄▄▒▒██▒▒▄▄▒▒█▒▒▄▄▒▒█████████▒▒▄▄▒▒█████████▒▒▄▄▒▒████▒▒▄▄▒▒███▒▒▄▄▒▒██▒▒▄▄▒▒███▒▒▄▄▒▒██▒▒▄▄▒▒█████▒▒▄▄▒▒████
+█▒▒▄▄▒▒██▒▒▄▄▒▒█▒▒▄▄▒▒▒▒▒▒▒▒▒▒█▒▒▄▄▒▒▒▒▒▒▒▒▒▒█▒▒▄▄▒▒▒▒▒▒▒▒▄▄▒▒███▒▒▄▄▒▒▒▒▒▒▄▄▒▒▒▒█▒▒▄▄▒▒██▒▒▄▄▒▒█████▒▒▄▄▒▒████
+█▒▒▄▄▒▒██▒▒▄▄▒▒█▒▒▄▄▄▄▄▄▄▄▄▄▒▒█▒▒▄▄▄▄▄▄▄▄▄▄▒▒█▒▒▄▄▄▄▄▄▄▄▄▄▄▄▒▒███▒▒▄▄▄▄▄▄▄▄▄▄▄▄▒▒█▒▒▄▄▒▒██▒▒▄▄▒▒█████▒▒▄▄▒▒████
+█▒▒▄▄▒▒██▒▒▄▄▒▒█▒▒▒▒▒▒▒▒▒▒▄▄▒▒█▒▒▄▄▒▒▒▒▒▒▒▒▒▒█▒▒▄▄▒▒▒▒▒▒▄▄▒▒▒▒███▒▒▄▄▒▒▒▒▒▒▒▒▄▄▒▒█▒▒▄▄▒▒██▒▒▄▄▒▒█████▒▒▄▄▒▒████
+█▒▒▄▄▒▒██▒▒▄▄▒▒█████████▒▒▄▄▒▒█▒▒▄▄▒▒█████████▒▒▄▄▒▒██▒▒▄▄▒▒█████▒▒▄▄▒▒████▒▒▄▄▒▒█▒▒▄▄▒▒██▒▒▄▄▒▒█████▒▒▄▄▒▒████
+█▒▒▄▄▒▒▒▒▒▒▄▄▒▒█▒▒▒▒▒▒▒▒▒▒▄▄▒▒█▒▒▄▄▒▒▒▒▒▒▒▒▒▒█▒▒▄▄▒▒██▒▒▄▄▒▒▒▒▒▒█▒▒▄▄▒▒▒▒▒▒▒▒▄▄▒▒█▒▒▄▄▒▒▒▒▒▒▄▄▒▒█████▒▒▄▄▒▒████
+█▒▒▄▄▄▄▄▄▄▄▄▄▒▒█▒▒▄▄▄▄▄▄▄▄▄▄▒▒█▒▒▄▄▄▄▄▄▄▄▄▄▒▒█▒▒▄▄▒▒██▒▒▄▄▄▄▄▄▒▒█▒▒▄▄▄▄▄▄▄▄▄▄▄▄▒▒█▒▒▄▄▄▄▄▄▄▄▄▄▒▒█████▒▒▄▄▒▒████
+█▒▒▒▒▒▒▒▒▒▒▒▒▒▒█▒▒▒▒▒▒▒▒▒▒▒▒▒▒█▒▒▒▒▒▒▒▒▒▒▒▒▒▒█▒▒▒▒▒▒██▒▒▒▒▒▒▒▒▒▒█▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒█▒▒▒▒▒▒▒▒▒▒▒▒▒▒█████▒▒▒▒▒▒████
+███████████████████████████████████████████████████████████████████████████████████████████████████████████████
+
+
+                                                           
+
+"""
+
+async def add_bot(bot_token):
+    await bot.start(bot_token)
+    bot.me = await bot.get_me()
+    bot.uid = telethon.utils.get_peer_id(bot.me)
+
+
+async def startup_log_all_done():
+    try:
+        await bot.send_message(THUND, f"**kgbot has been started**")
+    except BaseException:
+        print("Either PRIVATE_GROUP_ID is wrong or you have left the group.")
+
+if len(argv) not in (1, 3, 4):
+    bot.disconnect()
+else:
+    bot.tgbot = None
+    if Var.TG_BOT_USER_NAME_BF_HER is not None:
+        print("Initiating Inline Bot")
+        bot.tgbot = TelegramClient(
+            "TG_BOT_TOKEN",
+            api_id=Var.APP_ID,
+            api_hash=Var.API_HASH
+        ).start(bot_token=Var.TG_BOT_TOKEN_BF_HER)
+        print("Initialisation finished, no errors")
+        print("Starting Userbot")
+        bot.loop.run_until_complete(add_bot(Var.TG_BOT_USER_NAME_BF_HER))
+        print("Startup Completed")
+    else:
+        bot.start()
+
+path = 'kgbot/plugins/*.py'
+files = glob.glob(path)
+for name in files:
+    with open(name) as f:
+        path1 = Path(f.name)
+        shortname = path1.stem
+        load_module(shortname.replace(".py", ""))
+
+print("kgbot has been deployed! ")
+
+print("Setting up Assisting Bot")
+path = "kgbot/plugins/lightningbot/*.py"
+files = glob.glob(path)
+for name in files:
+    with open(name) as f:
+        path1 = Path(f.name)
+        shortname = path1.stem
+        start_mybot(shortname.replace(".py", ""))
+
+if LOAD_MYBOT == "True":
+    path = "kgbot/plugins/lightningbot/pmsparkbot/*.py"
+    files = glob.glob(path)
+    for name in files:
+        with open(name) as f:
+            path1 = Path(f.name)
+            shortname = path1.stem
+            load_pmbot(shortname.replace(".py", ""))
+    print("Assisting Bot set up completely!")
+
+print(logo)
+print("kgbot has been started without any issue! Please visit @rakasupport on telegram for more")
+bot.loop.run_until_complete(startup_log_all_done())
+
+if len(argv) not in (1, 3, 4):
+    bot.disconnect()
+else:
+    bot.run_until_disconnected()
